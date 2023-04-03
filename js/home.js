9658404940f0ed046722849766f98e9d2970bb5c
LuckBM <116355056+LuckBM@users.noreply.github.com>
2023-04-02 22:50:10 -0300

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,12 @@
+// Exemplo de código JS para interagir com o site
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+	card.addEventListener('click', () => {
+		// Ação ao clicar no card
+	});
+});
+
+
+
